7905e4f676234e0de2c294e7d024f682d9418279
wangsw <wangsw@ushareit.com>
2024-05-27 18:44:56 +0800

    feat: update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-// node 需要监听的dom node
-// options 配置项
-
 function intersection(node, options) {
   const { once = false, ...observerOptions } = options;
   let hasBeenVisible = false;
@@ -10,16 +7,17 @@ function intersection(node, options) {
       if (entry.isIntersecting) {
         if (once && hasBeenVisible) return;
         node.dispatchEvent(new CustomEvent('enterViewport'));
-        if (once) {
+        console.log('Element has entered the viewport---');
+        if (once && !hasBeenVisible) {
           hasBeenVisible = true;
+          observer.unobserve(node); // 停止观察
         }
       } else {
-        if (once && hasBeenDisVisible) return;
+        console.log(once, hasBeenVisible);
         // 如果是一次曝光，并且已经曝过
         node.dispatchEvent(new CustomEvent('exitViewport'));
         if (once) {
           hasBeenDisVisible = true;
-          observer.unobserve(node); // 停止观察
         }
       }
     });
